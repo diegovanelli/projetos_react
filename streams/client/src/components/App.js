74e7e98b040a824a1c962664c75dc21fbe9c4196
diegovanelli <diegodvanelli@gmail.com>
2019-09-03 05:32:11 -0300

    Add projects

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
-import Header from './Header'
+import Header from './Header';
+import history from '../history';
 
 
 class App extends React.Component {
     render() { 
         return ( 
             <div className="ui container">
-                <BrowserRouter>
+                <Router history={history}>
                     <Header />
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
-                </BrowserRouter>
+                    <Switch>
+                        <Route path='/' exact component={StreamList} />
+                        <Route path='/streams/new' exact component={StreamCreate} />
+                        <Route path='/streams/edit/:id' exact component={StreamEdit} />
+                        <Route path='/streams/delete/:id' exact component={StreamDelete} />
+                        <Route path='/streams/:id' exact component={StreamShow} />
+                    </Switch>
+                </Router>
             </div>
          );
     }
--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
@@ -11,12 +12,18 @@ class StreamList extends React.Component {
         if (stream.userId === this.props.currentUserId) {
             return (
                 <div className='right floated content'>
-                    <button className='ui button primary'>
+                    <Link
+                        className='ui button primary'
+                        to={`/streams/edit/${stream.id}`}
+                    >
                         Edit
-                    </button>
-                    <button className='ui button negative'>
+                    </Link>
+                    <Link
+                        className='ui button negative'
+                        to={`/streams/delete/${stream.id}`}
+                    >
                         Delete
-                    </button>
+                    </Link>
                 </div>
             );
         }
@@ -29,7 +36,9 @@ class StreamList extends React.Component {
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`} className='header' >
+                            {stream.title}
+                        </Link>
                         <div className='description'>{stream.description}</div>
                     </div>
                 </div>
@@ -37,11 +46,24 @@ class StreamList extends React.Component {
         })
     }
 
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right'}}>
+                    <Link to='/streams/new' className='ui button primary'>
+                        Create Stream
+                    </Link>
+                </div>            
+            );
+        }
+    }
+
     render() {
         return (
             <div>
                 <h2>Streams</h2>
                 <div className='ui celled list'>{this.renderList()}</div>
+                {this.renderCreate()}
             </div>
         );
     }
@@ -51,6 +73,7 @@ const mapStateToProps = (state) => {
     return {
         streams: Object.values(state.streams),
         currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn,
     }
 }
 
